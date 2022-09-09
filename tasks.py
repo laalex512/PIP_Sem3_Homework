51c2a86c20fa627852980291a28f154ded34992e
laalex <laalex512@gmail.com>
2022-09-09 19:35:38 +0300

    Add Task23

--- tasks.py
+++ tasks.py
@@ -14,3 +14,26 @@
 #         sum += lst[i]
 # print(lst)
 # print(sum)
+
+
+# Task 23
+# Напишите программу, которая найдёт произведение пар чисел списка.
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+# Пример:
+
+# - [2, 3, 4, 5, 6] => [12, 15, 16];
+# - [2, 3, 5, 6] => [12, 15]
+
+# from random import randint
+
+
+# n = int(input("Insert coun of list: "))
+# initList = [0]*n
+# for i in range(n):
+#     initList[i] = randint(0, 10)
+# print(initList)
+# resultList = []
+# for i in range(round(n/2+0.25)):
+#     resultList.append(initList[i]*initList[n-1-i])
+# print(resultList)
