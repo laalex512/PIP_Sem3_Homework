70cd5f22044daba477edff90ad4527e3945b8057
laalex <laalex512@gmail.com>
2022-09-09 20:10:59 +0300

    Add Task26

--- tasks.py
+++ tasks.py
@@ -83,3 +83,26 @@
 #     result = str(n % 2) + result
 #     n = n//2
 # print(result)
+
+
+# Task 26
+# Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+
+# Пример:
+
+# - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+
+# k = int(input("Inset k: "))
+
+# fibonacciList = [0]*(k*2+1)
+# fibonacciList[k] = 0
+# fibonacciList[k+1] = 1
+
+# for i in range(k+2, len(fibonacciList)):
+#     fibonacciList[i] = fibonacciList[i-2]+fibonacciList[i-1]
+
+# for i in range(k, -1, -1):
+#     fibonacciList[i] = fibonacciList[i+2]-fibonacciList[i+1]
+
+# print(fibonacciList)
